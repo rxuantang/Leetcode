135b24e4ddb26bf9ff10961d1c3e7924cae370fa
rxuantang <276703362+rxuantang@users.noreply.github.com>
2026-08-25 12:13:36 -0400

    solve: reverse single linked list (two pointers)

--- 204_reverse_linked_list_single_linked_list_two_pointers.py
+++ 204_reverse_linked_list_single_linked_list_two_pointers.py
@@ -14,3 +14,27 @@ class Solution:
             current_pointer = temp_pointer
         
         return pre_pointer
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Test case 1
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    new_head = solution.reverseList(head)
+    result = []
+    while new_head:
+        result.append(new_head.val)
+        new_head = new_head.next
+    assert result == [5, 4, 3, 2, 1]
+
+    # Test case 2
+    head = None
+    new_head = solution.reverseList(head)
+    assert new_head is None
+
+    # Test case 3
+    head = ListNode(1)
+    new_head = solution.reverseList(head)
+    assert new_head.val == 1 and new_head.next is None
+
+    print("passed all test cases!")
